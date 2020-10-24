ac4e3bae9248a35ccd25bfdd4402ba7b3ef9d862
JokinAce <pisferko@gmail.com>
2020-10-24 18:54:40 +0000

    Add files via upload

--- HiddenDeep/Program.cs
+++ HiddenDeep/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -24,9 +25,12 @@ namespace HiddenDeep
         {
             int Current = jokin.ReadInt((int)(jokin.BaseAddr() + Base));
 
-            foreach (int Pointer in Pointers)
+            if (Pointers != null)
             {
-                Current = jokin.ReadInt(Current + Pointer);
+                foreach (int Pointer in Pointers)
+                {
+                    Current = jokin.ReadInt(Current + Pointer);
+                }
             }
             return Current;
         }
@@ -40,6 +44,7 @@ namespace HiddenDeep
             Console.WriteLine("ALT+5 | Magnum Ammo 9999");
             Console.WriteLine("ALT+6 | Scan Balls 9999");
             Console.WriteLine("ALT+7 | Explosive Charges 9999");
+            Console.WriteLine("ALT+8 | HP % 9999");
             Console.WriteLine("-------------------------------------------");
 
             while (true)
@@ -48,19 +53,30 @@ namespace HiddenDeep
                 {
                     int MagnumAmmo = MultiPointer(0x03EB5D8, new int[] { 0x1BC, 0x8, 0x1A0, 0x24, 0x1C }); //0x40
                     jokin.WriteInt(MagnumAmmo + 0x40, 9999);
+                    Console.WriteLine("Magnum Ammo 9999");
                 }
 
                 if (IsKeyPushedDown(Keys.LMenu) & IsKeyPushedDown(Keys.D6))
                 {
                     int ScanBallsAmmo = MultiPointer(0x03EB5D8, new int[] { 0x1A0, 0xC, 0x28 }); //0x14
                     jokin.WriteInt(ScanBallsAmmo + 0x14, 9999);
+                    Console.WriteLine("Scan Balls 9999");
                 }
 
                 if (IsKeyPushedDown(Keys.LMenu) & IsKeyPushedDown(Keys.D7))
                 {
                     int ExplosivesAmmo = MultiPointer(0x03EB5D8, new int[] { 0x1A0, 0x30, 0x114 }); //0x14
                     jokin.WriteInt(ExplosivesAmmo + 0x14, 9999);
+                    Console.WriteLine("Explosive Charges 9999");
                 }
+
+                if (IsKeyPushedDown(Keys.LMenu) & IsKeyPushedDown(Keys.D8))
+                {
+                    int HP = jokin.ReadInt((int)(jokin.BaseAddr() + 0x03EB5D8));
+                    jokin.WriteFloat(HP + 0x1B0, 9999);
+                    Console.WriteLine("HP 9999");
+                }
+                Thread.Sleep(100);
             }
         }
     }
